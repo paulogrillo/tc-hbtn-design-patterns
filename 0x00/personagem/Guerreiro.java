874e56568692595acea235283b33c338b61eeb1b
Paulo Grillo <paulodigitalart@gmail.com>
2022-07-07 21:06:12 -0300

    fix

--- 0x00/personagem/Guerreiro.java
+++ 0x00/personagem/Guerreiro.java
@@ -1,7 +1,7 @@
 public class Guerreiro extends Personagem {
     public Guerreiro(String nome, int inteligencia, int forca, int vigor, int resistencia, int destreza){
         super(nome, TipoPersonagem.GUERREIRO, inteligencia, forca, vigor, resistencia, destreza);
-        if(forca < inteligencia || forca < destreza){
+        if(forca < inteligencia && forca < destreza){
             throw new RuntimeException("Atributos invalidos para GUERREIRO");
         }
     }
--- 0x00/personagem/Program.java
+++ 0x00/personagem/Program.java
@@ -1,5 +1,8 @@
+import java.util.Locale;
+
 public class Program {
     public static void main(String[] args) {
+        Locale.setDefault(new Locale("pt", "BR"));
         PersonagemFactory terraMediaPersonagemFactory = new WesterosPersonagemFactory();
         PersonagemFactory westerosPersonagemFactory = new TerraMediaPersonagemFactory();
 
