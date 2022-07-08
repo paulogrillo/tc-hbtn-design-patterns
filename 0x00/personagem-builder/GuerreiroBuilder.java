b53a2ff1bb3060d589bc64fb110c043d397c61ee
Paulo Grillo <paulodigitalart@gmail.com>
2022-07-07 22:02:27 -0300

    Task 01 Pattern Builder

---
+++ 0x00/personagem-builder/GuerreiroBuilder.java
@@ -0,0 +1,76 @@
+public class GuerreiroBuilder implements   Builder{
+    private String nome;
+    private TipoPersonagem tipo;
+    private int inteligencia;
+    private int forca;
+    private int vigor;
+    private int resistencia;
+    private int destreza;
+
+    public String getNome() {
+        return nome;
+    }
+
+    @Override
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public TipoPersonagem getTipo() {
+        return tipo;
+    }
+
+    @Override
+    public void setTipo(TipoPersonagem tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getInteligencia() {
+        return inteligencia;
+    }
+
+    @Override
+    public void setInteligencia(int inteligencia) {
+        this.inteligencia = inteligencia;
+    }
+
+    public int getForca() {
+        return forca;
+    }
+
+    @Override
+    public void setForca(int forca) {
+        this.forca = forca;
+    }
+
+    public int getVigor() {
+        return vigor;
+    }
+
+    @Override
+    public void setVigor(int vigor) {
+        this.vigor = vigor;
+    }
+
+    public int getResistencia() {
+        return resistencia;
+    }
+
+    @Override
+    public void setResistencia(int resistencia) {
+        this.resistencia = resistencia;
+    }
+
+    public int getDestreza() {
+        return destreza;
+    }
+
+    @Override
+    public void setDestreza(int destreza) {
+        this.destreza = destreza;
+    }
+
+    public Guerreiro build() {
+        return new Guerreiro(nome, inteligencia, forca, vigor, resistencia, destreza);
+    }
+}
---
+++ 0x00/personagem-builder/Ladrao.java
@@ -0,0 +1,16 @@
+public class Ladrao extends Personagem{
+
+
+    public Ladrao(String nome, int inteligencia, int forca, int vigor, int resistencia, int destreza) {
+        super(nome, TipoPersonagem.LADRAO, inteligencia, forca, vigor, resistencia, destreza);
+
+        if(destreza < forca || destreza < inteligencia){
+            throw new RuntimeException("Atributos invalidos para LADRAO");
+        }
+    }
+
+    @Override
+    public double getDanoAtaque() {
+        return (getForca() * 0.5) + (getDestreza() * 0.35) + (getVigor() * 0.1) + (getInteligencia() * 0.05);
+    }
+}
